fd9b88b742fb0e742f3cc68b76c12c7832abc280
Aisah Tan <aisahtan99@gmail.com>
2021-03-18 23:38:12 +0800

    Location Commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,15 @@
+package samples.demo;
+
+public class Location {
+	
+private String fname;
+	
+	public String getFname() {
+		return this.fname;
+	}
+	
+	public void setFname(String name) {
+		this.fname = name;
+	}
+
+}
