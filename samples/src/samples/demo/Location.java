c91f72c861459c9b502ecb3aeb2caa5ae73a9082
Aisah Tan <aisahtan99@gmail.com>
2021-03-18 23:39:13 +0800

    Location Commit 2

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -3,7 +3,10 @@ package samples.demo;
 public class Location {
 	
 private String fname;
-	
+private int locationId;
+private int description;
+
+
 	public String getFname() {
 		return this.fname;
 	}
