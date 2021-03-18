0ff3c632ee80e7a0a3bcffa2989268489821ab7d
Aisah Tan <aisahtan99@gmail.com>
2021-03-18 23:35:27 +0800

    Initial Commit

--- samples/src/samples/demo/GitTest.java
+++ samples/src/samples/demo/GitTest.java
@@ -2,9 +2,9 @@ package samples.demo;
 
 public class GitTest {
 	
-	public static void main(String[] args){
+public static void main(String[] args){
 		
-		System.out.print("Git is cool");
+		System.out.print("Git is fun");
 	}
 
 }
